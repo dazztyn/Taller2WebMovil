fd59857d59a909f40adb50733f5a5913c131f850
Leomax43 <130073563+Leomax43@users.noreply.github.com>
2025-11-15 17:45:24 -0300

    3 apis funcando
    3 apis que funcionan

--- api-express/src/index.js
+++ api-express/src/index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
   res.send('Hola esta es la dazztynApi');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   console.log("DB Config:", {
